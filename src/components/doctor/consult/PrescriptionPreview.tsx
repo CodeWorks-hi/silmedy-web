509ab89c30d4fb00a3b4c3471ad450f18a35a26c
nhchoi2 <choi1278@gmail.com>
2025-05-11 00:22:22 +0900

    Update PrescriptionPreview.tsx

--- src/components/doctor/consult/PrescriptionPreview.tsx
+++ src/components/doctor/consult/PrescriptionPreview.tsx
@@ -41,9 +41,8 @@ export default function PrescriptionPreview({
             year: 'numeric',
             month: '2-digit',
             day: '2-digit',
-        })
-        : '';
-
+        }) : '';
+    const uniqueDiseases = Array.from(new Set(prescriptions.map((p) => p.disease)));
     return (
         <div className="p-6" id="prescription-preview">
             {/* 타이틀 */}
@@ -52,44 +51,27 @@ export default function PrescriptionPreview({
             </h3>
 
             {/* 병원·의사·환자 정보 테이블 */}
-            <table className="border w-full text-sm">
-            <colgroup>
-                <col style={{ width: '20%' }} />
-                <col style={{ width: '50%' }} />
-
-
-            </colgroup>
+            <table className="border w-full text-xs text-center font-medium">
+                <colgroup>
+                    <col style={{ width: '20%' }} />
+                    <col style={{ width: '80%' }} />
+                </colgroup>
                 <tr>
-                    <th className="bg-cyan-200 border px-3 py-2 text-center font-medium">교부번호</th>
-                    <th className="border px-3 py-2 text-center font-medium">{formatDate(today)} 제 {prescriptionId ?? '-'} 호</th>
-
+                    <th className="bg-gray-100 py-3">교부번호</th>
+                    <td>{formatDate(today)} 제 {prescriptionId ?? '-'} 호</td>
                 </tr>
             </table>
-            <table className="w-full text-sm">
-            <colgroup>
-                <col style={{ width: '50%' }} />
-                <col style={{ width: '50%' }} />
-            </colgroup>
+            <table className="border w-full text-xs text-center">
+                <colgroup>
+                    <col style={{ width: '50%' }} />
+                    <col style={{ width: '50%' }} />
+                </colgroup>
                 <tr className="bg-gray-100">
-                    <th className="border px-3 py-2 text-center font-medium">환&nbsp;자</th>
-                    <th className="border px-3 py-2 text-center font-medium">의료기관</th>
+                    <th className="py-3">환&nbsp;자</th>
+                    <th>의료기관</th>
                 </tr>
             </table>
-            <table className="w-full text-sm">
-            <colgroup>
-                <col style={{ width: '20%' }} />
-                <col style={{ width: '30%' }} />
-                <col style={{ width: '20%' }} />
-                <col style={{ width: '30%' }} />
-            </colgroup>
-                <tr>
-                    <td className="bg-gray-100 border px-3 py-2 text-center font-medium">성명</td>
-                    <td className="border px-3 py-2 text-center font-medium">{patientName}</td>
-                    <td className="bg-gray-100 border px-3 py-2 text-center font-medium">기관명</td>
-                    <td className="border px-3 py-2 text-center font-medium">{hospitalName}</td>
-                </tr>
-            </table>
-            <table className="w-full text-sm">
+            <table className="w-full text-sm text-center text-xs ">
                 <colgroup>
                     <col style={{ width: '20%' }} />
                     <col style={{ width: '30%' }} />
@@ -97,74 +79,83 @@ export default function PrescriptionPreview({
                     <col style={{ width: '30%' }} />
                 </colgroup>
                 <tr>
-                   
-                    <td className="bg-gray-100 border px-3 py-2 text-center font-medium">생년월일</td>
-                    <td className="border px-3 py-2 text-center font-medium">{formattedBirth}</td>
-                    <td className="bg-gray-100 border px-3 py-2 text-center font-medium">연락처</td>
-                    <td className="border px-3 py-2 text-center font-medium">{hospitalContact}</td>
+                    <th className="bg-gray-100 border px-3 py-2">성명</th>
+                    <td className="border px-3 py-2 text-center">{patientName}</td>
+                    <th className="bg-gray-100 border px-3 py-2">기관명</th>
+                    <td className="border px-3 py-2 text-center">{hospitalName}</td>
+                </tr>
+
+                <tr>
+                <th className="bg-gray-100 border px-3 py-2 text-center">생년월일</th>
+                    <td className="border px-3 py-2 text-center">{formattedBirth}</td>
+                    <th className="bg-gray-100 border px-3 py-2 text-center">연락처</th>
+                    <td className="border px-3 py-2 text-center">{hospitalContact}</td>
                 </tr>
             </table>
-            <table className="w-full mb-6 text-sm">
-                <tbody>
+            {/* 처방전 테이블 */}
+            <table className="border w-full text-xs text-center">
+                <colgroup>
+                    <col style={{ width: '8%' }} />
+                    <col style={{ width: '15%' }} />
+                    <col style={{ width: '15%' }} />
+                    <col style={{ width: '20%' }} />
+                    <col style={{ width: '15%' }} />
+                    <col style={{ width: '20%' }} />
+                </colgroup>
+                <thead>
+                    <tr>
+                        <th rowSpan={prescriptions.length + 2} className="bg-gray-100 border px-2 py-1 text-center">
+                            질병코드
+                        </th>
 
-                    <tr className="bg-gray-50">
+                        <td className="border px-2 py-1 text-center">
+                            {uniqueDiseases[0]}
+                        </td>
 
-                        <th className="px-3 py-2 text-right font-medium">보건소</th>
-                        <td className="px-3 py-2"></td>
-                        <th className="px-3 py-2 text-right font-medium">주소</th>
-                        <td className="px-3 py-2">{hospitalAddress}</td>
-                        <th className="px-3 py-2 text-right font-medium">연락처</th>
-                        <td className="px-3 py-2"></td>
+                        <th rowSpan={prescriptions.length + 2} className="bg-gray-100 border px-3 py-2 text-center font-medium">처방의료인의성명</th>
+                        <td rowSpan={prescriptions.length + 2} className="border px-3 py-2 text-center">{doctorName}</td>
+
+                        <th className="bg-gray-100 border px-3 py-2 text-center font-medium">면허종별</th>
+                        <td className="border px-3 py-2 text-center">의 &nbsp; &nbsp; &nbsp;사</td>
                     </tr>
                     <tr>
-                        <th className="px-3 py-2 text-right font-medium">의사명</th>
-                        <td className="px-3 py-2">{doctorName}</td>
-                        <th className="px-3 py-2 text-right font-medium">면허번호</th>
-                        <td className="px-3 py-2">{licenseNumber}</td>
-                        <th className="px-3 py-2 text-right font-medium">환자명</th>
-                        <td className="px-3 py-2">{patientName}</td>
+                        <td className="border px-2 py-1 text-center">
+                            {uniqueDiseases[1]}
+                        </td>
+                        <th className="bg-gray-100 border px-3 py-2 text-center font-medium">면허번호</th>
+                        <td className="border px-3 py-2 text-center">{licenseNumber}</td>
                     </tr>
-                </tbody>
-            </table>
 
-            {/* 처방전 테이블 */}
-            <table className="w-full border-collapse text-xs">
-                <thead>
+                </thead>
+            </table>
+            
+            <table className="border w-full text-xs text-center">
                     <tr className="bg-gray-100">
-                        {[
-                            '병명 코드',
-                            '의약품',
-                            '투약량',
-                            '투약회수',
-                            '용법',
-                            '일수',
+                        {['의약품', '투약량', '투약회수', '용법', '일수',
                         ].map((header) => (
                             <th
                                 key={header}
-                                className="border px-2 py-1 text-center font-medium"
-                            >
+                                className="border px-2 py-3">
                                 {header}
                             </th>
                         ))}
                     </tr>
-                </thead>
-                <tbody>
-                    {prescriptions.map((p, i) => (
-                        <tr
-                            key={i}
-                            className={i % 2 === 0 ? 'bg-white' : 'bg-gray-50'}
-                        >
-                            <td className="border px-2 py-1 text-center">{p.disease}</td>
-                            <td className="border px-2 py-1">{p.drug}</td>
-                            <td className="border px-2 py-1 text-center">{p.amount}</td>
-                            <td className="border px-2 py-1 text-center">
-                                {p.frequency}
-                            </td>
-                            <td className="border px-2 py-1 text-center">{p.method}</td>
-                            <td className="border px-2 py-1 text-center">{p.days}</td>
-                        </tr>
-                    ))}
-                </tbody>
+                    <tbody>
+                        {prescriptions.map((p, i) => (
+                            <tr key={i} className={i % 2 === 0 ? 'bg-white' : 'bg-white'}>
+                                <td className="border px-2 py-1">{p.drug}</td>
+                                <td className="border px-2 py-1 text-center">{p.amount}</td>
+                                <td className="border px-2 py-1 text-center">{p.frequency}</td>
+                                <td className="border px-2 py-1 text-center">{p.method}</td>
+                                <td className="border px-2 py-1 text-center">{p.days}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+            </table>
+            <table className="w-full h-5 text-center font-medium text-xs ">
+            <td className="text-center px-3 py-2 text-xs italic">
+                  해당 처방전은 실제 사용이 불가능하며, 이해를 돕기 위한 화면입니다.
+                </td>
             </table>
         </div>
     );
